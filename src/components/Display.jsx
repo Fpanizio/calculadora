9c138751b31a17318b89fe86cd7398813fa5e915
Felipe Panizio <felipe.yudi@ykt.com.br>
2021-12-07 14:31:27 -0300

    Fisnish

---
+++ src/components/Display.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import "./Display.css";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (porps) => <div className="display">{porps.value}</div>;
